bbda9dbbace165a948cf83a7f491c3d9ea915137
joaquinkun <66292702+joaquinkun@users.noreply.github.com>
2021-10-19 04:31:43 -0300

    arreglando

--- javascript/js.js
+++ javascript/js.js
@@ -3,22 +3,22 @@ let discount;
 let valueDot;
 let totalResult = ticketValue;
 const result = document.querySelector(".alert");
-const cards = document.querySelectorAll(".border")
+const cards = document.querySelectorAll(".border");
 document.querySelector(".form-number").addEventListener("change", e => {
     valueDot = e.target.value
     change()
 })
 
 document.querySelector(".form-select").addEventListener("change", (e) =>{
-    discount = e.target.value
-    change()
+    discount = e.target.value;
+    change();
+    setColorCard();
 })
 
 function change(){
     if(valueDot != undefined && valueDot > 0 && valueDot <=4){
         totalResult = ticketValue*valueDot;
         if(discount > 0){
-            setColorCard()
             let totalResultDot = totalResult;
             totalResult = -totalResult*discount/100 + totalResult;
             result.innerHTML = `Total a Pagar: <b>${totalResult}</b> (<strike>${totalResultDot}</strike> <b>%${discount} OFF!</b>)`
@@ -26,25 +26,28 @@ function change(){
             result.innerHTML = `Total a Pagar: <b>${totalResult}</b>`
         }
     }else{
-        setColorCard()
         result.innerHTML = `Total a Pagar: <b>----</b>`
     }
 }
 
 function setColorCard(){
     cards.forEach(e => {
-        console.log(e)
         if(discount==e.id && e.classList.contains("toggle") == false){
             e.classList.add("toggle");
+            console.log(e.classList.contains("toggle"));
         }else{
             e.classList.remove("toggle");
         }
     })
 }
 
-function clickReset(e){
-    if(confirm("Seguro que quieres borrar?")){
-        e.setAttribute("type","reset");
-        result.innerHTML = "Total a Pagar: ";
+document.querySelector(".delete").addEventListener("click", e => {
+    e.target.removeAttribute("type","reset");
+    let r = confirm("¿Seguro que quieres borrar?");
+    if(r){
+        e.target.setAttribute("type","reset");
+        setColorCard();
+    }else{
+        e.preventDefault();
     }
-}
+})
